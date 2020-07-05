5d06b85d741fc422cc52fd04e514b37359237f06
Jay <2033066030@qq.com>
2020-07-05 14:43:50 +0800

    增加server公共模块，测试MyBatis查询

--- system/src/main/java/com/course/system/controller/TestController.java
+++ system/src/main/java/com/course/system/controller/TestController.java
@@ -1,13 +1,27 @@
 package com.course.system.controller;
 
+import com.course.server.service.TestService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 @RestController
 public class TestController {
 
+    @Resource
+    private TestService testService;
+
     @RequestMapping("/test")
     public String success(){
         return "success";
+    }
+
+    @GetMapping("query.do")
+    @ResponseBody
+    public Object query(){
+        return testService.list();
     }
 }
